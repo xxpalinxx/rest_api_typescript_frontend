f2d26206a16bb372724e83d8b6a3052df82bc6d5
xxpalinxx <hkpalin@gmail.com>
2025-02-18 02:14:08 -0300

    sacando output de valibot

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-import { object, string, number, boolean, Output, array } from 'valibot'
+import { object, string, number, boolean, array } from 'valibot'
 
 export const DraftProductSchema = object({
     name: string(),
@@ -13,4 +13,9 @@ export const ProductSchema = object({
 })
 
 export const ProductsSchema = array(ProductSchema)
-export type Product = Output<typeof ProductSchema>
+export type Product = {
+    id: number;
+    name: string;
+    price: number;
+    availability: boolean;
+}
